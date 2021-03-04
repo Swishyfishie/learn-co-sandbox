f36bd60555d8128142f5c1550f44938fc984ee4f
Andrei Pestenariu <pclandrei@gmail.com>
2021-03-04 19:00:56 +0000

    Automatically backed up by Learn

---
+++ demo.rb
@@ -0,0 +1,18 @@
+banana = {
+  :item => "banana",
+  :price => "6",
+  :description =>"fruit"
+}
+
+ banana = {
+  item:"banana",
+  price: 62,
+  description: "fruit",
+  weight: "55Kg"
+}
+
+if banana[:weight]
+  puts 'IT has a weight'
+else
+  puts "It doesn't have a weight"
+end  
